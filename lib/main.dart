c48b28f4a9dbd67acbee82559b47ab481c2d9d82
lsh <sdsd@sdsd.com>
2024-08-05 21:44:52 +0900

    제 1강 커밋 ver.0

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('기본위젯'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Icon(
+            Icons.favorite,
+            size: 100,
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
