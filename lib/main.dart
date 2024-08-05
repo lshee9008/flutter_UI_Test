ae27f5944f01d8be5ac6fb066489930512def747
lsh <sdsd@sdsd.com>
2024-08-05 23:41:09 +0900

    2 장 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,44 @@ class MyApp extends StatelessWidget {
           title: Text('기본위젯'),
           backgroundColor: Colors.blue,
         ),
-        body: Center(
-          child: Icon(
-            Icons.favorite,
-            size: 100,
-            color: Colors.red,
+        body: Container(
+          /* 앱 화면의 maximum 사이즈 값 설정*/
+          // width: double.infinity,
+          // height: 500,
+          color: Colors.yellow[200],
+
+          // margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+
+          // margin: EdgeInsets.symmetric(
+          //   vertical: 10,
+          //   horizontal: 10,
+          // ),
+
+          // margin: EdgeInsets.all(50),
+
+          // padding: EdgeInsets.all(100),
+
+          // decoration: BoxDecoration(
+          //   border: Border.all(
+          //     color: Colors.black,
+          //   ),
+          // ),
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              FlutterLogo(
+                size: 50,
+              ),
+              FlutterLogo(
+                size: 50,
+              ),
+              FlutterLogo(
+                size: 50,
+              ),
+            ],
           ),
         ),
       ),
